226499a3d65eff6668d502440307f16f92d30e5b
William Petrik <petrikw@bc.edu>
2026-05-23 15:03:24 -0400

    built experience cards

--- personal-website/app/layout.tsx
+++ personal-website/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
+import { Kaisei_Decol } from "next/font/google";
 import "./globals.css";
 import Navbar from "./_components/Navbar";
+
+const kaiseiDecol = Kaisei_Decol({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-kaisei",
+});
 
 export const metadata: Metadata = {
   title: "William Petrik",
@@ -13,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={kaiseiDecol.variable}>
       <body>
         <Navbar></Navbar>
         <div style={{ marginLeft: "6rem" }}>{children}</div>
--- personal-website/app/page.tsx
+++ personal-website/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import ExperienceCard from "./_components/ExperienceCard";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <h1>William Petrik</h1>
       <p>Welcome to my personal website where I get to show off what I have learned throughout my early career. I have worked as a climbing instructor, route setter, and customer service associate. <Link href="/about">Learn more about me here!</Link></p>
       <p>Feel free to explore and find out more about me and some of the experiences I have had.</p>
+      <ExperienceCard></ExperienceCard>
     </main>
   );
 }
