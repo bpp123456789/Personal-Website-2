42521e0559fafafef9faffe528aff82b2044a2ce
William Petrik <petrikw@bc.edu>
2026-05-23 23:15:51 -0400

    Added the news section to my home page

--- personal-website/app/page.tsx
+++ personal-website/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ExperienceCard from "./_components/ExperienceCard";
+import NewsList from "./_components/NewsCard";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <p>Welcome to my personal website where I get to show off what I have learned throughout my early career. I have worked as a climbing instructor, route setter, and customer service associate. <Link href="/about">Learn more about me here!</Link></p>
       <p>Feel free to explore and find out more about me and some of the experiences I have had.</p>
       <ExperienceCard></ExperienceCard>
+      <NewsList></NewsList>
     </main>
   );
 }
